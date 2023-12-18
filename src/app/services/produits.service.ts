1c08474a44a147ddb921e314cb5433e3822e4eaa
Ameni14101998 <ameni.benrbiha14@gmail.com>
2023-12-18 18:53:23 +0100

    Premier commit avec mon projet

---
+++ src/app/services/produits.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produit } from '../model/produit';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitsService {
+  // Url du service web de gestion de produits
+  // commune pour toutes les méthodes
+  urlHote = "http://localhost:3333/produits/";
+
+  constructor(private http: HttpClient) { }
+
+  getProduits(): Observable<Array<Produit>> {
+    return this.http.get<Array<Produit>>(this.urlHote);
+  }
+
+  deleteProduit(idP: number | undefined): Observable<void> {
+    return this.http.delete<void>(this.urlHote + idP);
+  }
+
+  addProduit(nouveau: Produit): Observable<Array<Produit>> {
+    return this.http.post<Array<Produit>>(this.urlHote, nouveau);
+  }
+
+  updateProduit(idP: number | undefined, nouveau: Produit): Observable<void> {
+    return this.http.put<void>(this.urlHote + idP, nouveau);
+  }
+  
+  getProduitsParCategorie(categorieId: number): Observable<Produit[]> {
+    return this.http.get<Produit[]>(`${this.urlHote}par-categorie/${categorieId}`);
+  }
+}
